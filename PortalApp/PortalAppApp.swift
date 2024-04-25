bfea7f20634f8c755538a98e53534eb627a3c7b6
A person <Nothing@nothing.co>
2024-04-25 18:46:30 +0800

    Initial Commit

---
+++ PortalApp/PortalAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortalAppApp.swift
+//  PortalApp
+//
+//  Created by Lai Hong Yu on 25/4/24.
+//
+
+import SwiftUI
+
+@main
+struct PortalAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
